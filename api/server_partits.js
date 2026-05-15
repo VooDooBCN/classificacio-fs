ce5a02fc71372284b0848d2ba5765048962af86f
VooDooBCN <98989929+VooDooBCN@users.noreply.github.com>
2026-05-15 23:21:07 +0200

    Add files via upload

--- api/server_partits.js
+++ api/server_partits.js
@@ -204,11 +204,34 @@ const visitant = equips.eq(1)
 // IGNORAR PARTITS JA JUGATS
 // =====================================
 
-if (
-  esPaginaEquip &&
-  /\d+\s*-\s*\d+/.test(marcador)
-) {
-  continue;
+const esResultat =
+  /\d+\s*-\s*\d+/.test(marcador);
+
+// si és resultat, mantenir-lo 48h
+if (!esPaginaEquip && esResultat) {
+
+  const match =
+    dataPartit.match(/(\d{2})[\/-](\d{2})[\/-](\d{4})/);
+
+  if (match) {
+
+    const [, d, m, y] = match;
+
+    const dataPartitObj =
+      new Date(`${y}-${m}-${d}`);
+
+    const ara = new Date();
+
+    const diferenciaDies =
+      (ara - dataPartitObj) / (1000 * 60 * 60 * 24);
+
+    // si han passat més de 2 dies
+    if (diferenciaDies > 2) {
+      continue;
+    }
+
+  }
+
 }
       // =====================================
       // SI ÉS /RESULTATS/
